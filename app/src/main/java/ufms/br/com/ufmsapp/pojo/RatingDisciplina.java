6747d4c288a7dfee61a557eaf2469407c6433282
luizhsda10 <luizhsda@gmail.com>
2016-01-20 01:35:49 -0300

    Refactor no banco de dados e Parser JSON.

---
+++ app/src/main/java/ufms/br/com/ufmsapp/pojo/RatingDisciplina.java
@@ -0,0 +1,63 @@
+package ufms.br.com.ufmsapp.pojo;
+
+
+public class RatingDisciplina {
+
+    int idRatingDisciplina;
+    int alunoKey;
+    int disciplinaKey;
+    float rating;
+
+    public RatingDisciplina() {
+    }
+
+    public RatingDisciplina(int idRatingDisciplina, int alunoKey, int disciplinaKey, float rating) {
+        this.idRatingDisciplina = idRatingDisciplina;
+        this.alunoKey = alunoKey;
+        this.disciplinaKey = disciplinaKey;
+        this.rating = rating;
+    }
+
+    public int getIdRatingDisciplina() {
+        return idRatingDisciplina;
+    }
+
+    public void setIdRatingDisciplina(int idRatingDisciplina) {
+        this.idRatingDisciplina = idRatingDisciplina;
+    }
+
+    public int getAlunoKey() {
+        return alunoKey;
+    }
+
+    public void setAlunoKey(int alunoKey) {
+        this.alunoKey = alunoKey;
+    }
+
+    public int getDisciplinaKey() {
+        return disciplinaKey;
+    }
+
+    public void setDisciplinaKey(int disciplinaKey) {
+        this.disciplinaKey = disciplinaKey;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "RatingDisciplina{" +
+                "idRatingDisciplina=" + idRatingDisciplina +
+                ", alunoKey=" + alunoKey +
+                ", disciplinaKey=" + disciplinaKey +
+                ", rating=" + rating +
+                '}';
+    }
+
+}
