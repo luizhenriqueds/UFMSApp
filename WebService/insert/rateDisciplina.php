f8902e1af0de5e8b7224d49dbf75d23553611096
luizhsda10 <luizhsda@gmail.com>
2016-01-22 01:40:48 -0300

    POST método web service.

---
+++ WebService/insert/rateDisciplina.php
@@ -0,0 +1,35 @@
+<?php
+
+include("../connection/Connect.php");
+
+$db = new Connect();
+
+$connection = $db->connect();
+
+
+$sql = "INSERT INTO app_rating_disciplina (app_aluno_key, app_disciplina_key, app_rating) VALUES (?, ?, ?)";
+
+$query = $connection->prepare($sql);
+
+/*$json = json_decode(file_get_contents('php://input'));
+$alunoKey = $json->{'alunoKey'};
+$disciplinaKey = $json->{'disciplinaKey'};
+$disciplinaRating = $json->{'disciplinaRating'};*/
+
+$alunoKey = $_POST['alunoKey'];
+$disciplinaKey = $_POST['disciplinaKey'];
+$disciplinaRating = $_POST['disciplinaRating'];
+
+$query->execute(array($alunoKey, $disciplinaKey, $disciplinaRating));
+
+$id = $connection->lastInsertId("app_id_rating_disciplina");
+
+$returnJson = array("id" => $id);
+
+header("Content-type: application/json");
+
+echo json_encode($returnJson);
+
+
+
+
